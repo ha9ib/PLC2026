5452474342972a5ad4b245ea6f5d35f2bcbd9f7b
ha9ib <sc9r@outlook.com>
2026-02-17 17:45:17 +0000

    2 task done added pause, show and pieces

--- lab5-records/haskell/playlist.hs
+++ lab5-records/haskell/playlist.hs
@@ -17,16 +17,21 @@ instance Show Person where
 
 data Item
     = Piece
-        {
-            item_name :: String,
-            item_performer :: Person,
-            item_length_secs :: Float
+        { item_name :: String
+        , item_performer :: Person
+        , item_length_secs :: Float
         }
+    | Pause
+        { item_length_secs :: Float }
     deriving (Eq)
 
-instance (Show Item) where
+
+instance Show Item where
     show (Piece name performer len) =
         printf "%s by %s (%.1fs)" name (show performer) len
+    show (Pause len) =
+        printf "Pause (%.1fs)" len
+
 
 piece1 =
     Piece
@@ -43,26 +48,25 @@ piece2 =
         item_performer = Person "Daniel Barenboim",
         item_length_secs = 16*60+49
     }
-  
-{-
+
 pause1 =
-    Pause
-    { 
-        item_length_secs = 5
-    }
--}
+    Pause { item_length_secs = 5 }
+
 
 main =
     do
-    -- putStrLn "piece1 and piece2 sorted by length:"
-    -- putStrLn $ show shorterPiece
-    -- putStrLn $ show longerPiece
-    putStr "piece1 = "
-    putStrLn $ show piece1
---    putStr "pause1 = "
---    putStrLn $ show pause1
+        let (shorterPiece, longerPiece) = sortTwoItems (piece1, piece2) -- TASK
 
--- ... = sortTwoItems (piece1, piece2) -- TASK
+        putStrLn "piece1 and piece2 sorted by length:"
+        putStrLn $ show shorterPiece
+        putStrLn $ show longerPiece
+        putStr "piece1 = "
+        putStrLn $ show piece1
+        putStr "pause1 = "
+        putStrLn $ show pause1
+
+
+
 
 sortTwoItems (item1, item2) = 
     if item_length_secs item1 <= item_length_secs item2
